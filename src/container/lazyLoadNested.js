df5eda60a060b395b92703520cf9603a75fae163
Andreas Tzionis <andy0n7@gmail.com>
2022-11-17 13:14:02 +0200

    cleanup lazy load

--- src/container/lazyLoadNested.js
+++ src/container/lazyLoadNested.js
@@ -1,38 +1,28 @@
 
-import React, { useRef, useLayoutEffect } from 'react'
+import React, { useRef, useLayoutEffect, useState } from 'react'
 
-// Lazy render the module until the nested module is fetched
-const readModule = (record, readPromise) => {
-  
-  if (record.status === 'fulfilled') return record.result
-  else if (record.status === 'rejected') throw record.result
-
-  if (!record.promise) {
-    record.promise = readPromise()
-      .then((v) => Object.assign(record, { status: 'fulfilled', promise: null, result: v }))
-  } else throw record.promise
-}
-
-const rendererModule = {}
-
-const lazyLoadNested = (loadNested) => {
+const lazyLoad = (loadNested) => {
 
   return () => {
     const containerRef = useRef(null)
     
-    const Nested = readModule(rendererModule, loadNested)
-    
-    useLayoutEffect(() => {
-      const root = Nested.ReactDOM.createRoot(containerRef.current)
+    const [root, setRoot] = useState()
+
+    const updateRoot = (m) => {
+      const _root = m.ReactDOM.createRoot(containerRef.current)
       
-      if (Nested) {
-        const Component = Nested.App.default
-        root.render(<Component/>)
-      }
-      return () => root.unmount()
-    }, [Nested])
+      const Component = m.App.default
+      _root.render(<Component/>)
+
+      setRoot(_root)
+    }
+
+    useLayoutEffect(() => {
+      if (!root) loadNested().then(updateRoot)
+      return () => root?.unmount()
+    }, [root])
 
     return <div ref={containerRef} />
   }
 }
-export default lazyLoadNested
+export { lazyLoad }
